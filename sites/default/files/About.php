8af9a32e1623d3201697bb1443170985a3b07b67
Keith Lamprecht <klamprecht@BlueFlare-MBP-4.local>
2016-04-02 22:56:53 -0400

    Update to Kinetiq Digital

---
+++ sites/default/files/About.php
@@ -0,0 +1,207 @@
+<div class="bkg-head">&nbsp;</div>
+<div class="row content-wrapper">
+	<div class="col s12 m6 l6"><img alt="" class="circle responsive-img float profile-img" data-picture-mapping="2_column" src="/sites/default/files/me%20copy%202.jpg" /></div>
+	<div class="col s12 m6 l6">
+		<div class="card">
+			<div class="card-content grey-text">
+				<div class="card-title grey-text">Keith Lamprecht
+					<div class="card-icon right"><i class="mdi-social-person"></i></div>
+				</div>
+				<p>I am a Cleveland based mobile first developer with a passion for clean design. I work on everything from web to mobile and I always try to keep up with the latest new tech. I love to play around with home automation and anything I can develop to make my life easier. When I&#39;m not sitting in front of my computer I&#39;m out on the water sailing or in the woods backpacking.</p>
+			</div>
+		</div>
+		<div class="card">
+			<div class="card-content grey-text">
+				<div class="card-title grey-text">Contact Me
+					<div class="card-icon right"><i class="mdi-social-public"></i></div>
+				</div>
+				<span class="icon-table"><a href="mailto:lamprkei@umich.edu"><img alt="" data-picture-mapping="10_column" src="/sites/default/files/social/email.png" /></a><a href="https://www.linkedin.com/in/lamprkei"><img alt="" data-picture-mapping="10_column" src="/sites/default/files/social/linkedin.png" /></a><a href="https://plus.google.com/+KeithLamprecht"><img alt="" data-picture-mapping="10_column" src="/sites/default/files/social/google-plus.png" /></a><a href="https://instagram.com/nixon506e/"><img alt="" data-picture-mapping="10_column" src="/sites/default/files/social/instagram.png" /></a><a href="https://twitter.com/Nixon506E"><img alt="" data-picture-mapping="10_column" src="/sites/default/files/social/twitter.png" /></a></span></div>
+		</div>
+	</div>
+	<div class="col s12 m12 l12">
+		<div class="card">
+			<div class="card-content grey-text">
+				<div class="card-title grey-text">Experience
+					<div class="card-icon right"><i class="mdi-hardware-computer"></i></div>
+				</div>
+			</div>
+			<ul class="collapsible" data-collapsible="accordion">
+				<li>
+					<div class="collapsible-header">
+						<div class="section card-content grey-text">
+							<div class="row valign-wrapper">
+								<div class="col s7 m9 l9">
+									<h5 class="text-grey text-darken-2">KINETiQ DIGITAL</h5>
+									<p class="text-grey text-lighten-2">Senior Mobile Developer</p>
+									<p>December &#39;15 - Present</p>
+								</div>
+								<div class="col s5 m3 l3"><img alt="" class="responsive-img valign" data-picture-mapping="2_column" src="/sites/default/files/kinetiq_logo.png" /></div>
+							</div>
+						</div>
+					</div>
+					<div class="collapsible-body">
+						<ul class="section card-content grey-text">
+							<li>Developing consumer facing mobile applications for a variety of clients</li>
+							<li>Working mostly with native solutions for mobile applications</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<div class="collapsible-header">
+						<div class="section card-content grey-text">
+							<div class="row valign-wrapper">
+								<div class="col s7 m9 l9">
+									<h5 class="text-grey text-darken-2">Recess Creative</h5>
+									<p class="text-grey text-lighten-2">Creative Technologist</p>
+									<p>April &#39;14 - December &#39;15</p>
+								</div>
+								<div class="col s5 m3 l3"><img alt="" class="responsive-img valign" data-picture-mapping="2_column" src="/sites/default/files/recess_logo.jpg" /></div>
+							</div>
+						</div>
+					</div>
+					<div class="collapsible-body">
+						<ul class="section card-content grey-text">
+							<li>Developing websites and mobile applications for a variety of markets</li>
+							<li>Working mostly with cross-platform solutions for mobile applications</li>
+							<li>Majority of development included Android, iOS, and Windows releases simultaneously</li>
+							<li>Often given lead architecture and development opportunity on web and mobile applications</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<div class="collapsible-header">
+						<div class="section card-content grey-text">
+							<div class="row valign-wrapper">
+								<div class="col s7 m9 l9">
+									<h5 class="text-grey text-darken-2">Rosetta Marketing</h5>
+									<p class="text-grey text-lighten-2">Mobile Developer</p>
+									<p>November &#39;11 - April &#39;14</p>
+								</div>
+								<div class="col s5 m3 l3"><img alt="" class="responsive-img valign" data-picture-mapping="2_column" src="/sites/default/files/rosetta_logo.jpg" /></div>
+							</div>
+						</div>
+					</div>
+					<div class="collapsible-body">
+						<ul class="section card-content grey-text">
+							<li>Developing mobile applications for use in marketing and sales of a wide range of markets</li>
+							<li>Developing web sites for brand promotion, information, and correspondence</li>
+							<li>Developed an SDK and mobile application for server side syncing, document creation and distribution
+								<ul>
+									<li>Over-the-air document distribution and updating</li>
+									<li>Syncing of user based favorites, ratings, &amp; task completion o Asynchronous server requests</li>
+									<li>Complex custom animated dynamic UI</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</li>
+				<li>
+					<div class="collapsible-header">
+						<div class="section card-content grey-text">
+							<div class="row valign-wrapper">
+								<div class="col s7 m9 l9">
+									<h5 class="text-grey text-darken-2"><nobr>U of M</nobr> Athletic Department / Alumni Association</h5>
+									<p class="text-grey text-lighten-2">Intern</p>
+									<p>August &#39;10 - May &#39;11</p>
+								</div>
+								<div class="col s5 m3 l3"><img alt="" class="responsive-img valign" data-picture-mapping="2_column" src="/sites/default/files/michigan_alumni_logo.jpg" /></div>
+							</div>
+						</div>
+					</div>
+					<div class="collapsible-body">
+						<ul class="section card-content grey-text">
+							<li>Development and maintenance of the department website and conceptual work on mobile ticketing applications</li>
+							<li>Program Staff for the University of Michigan Alumni Association (Aug. 2008 - May 2010)
+								<ul>
+									<li>Helped with various events during the school year and worked as a staff member at the Alumni Association&rsquo;s family summer camp in northern Michigan</li>
+								</ul>
+							</li>
+						</ul>
+					</div>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="col s12 m12 l12">
+		<div class="card">
+			<div class="card-image valign-wrapper" style="max-height: 200px;"><img class="valign" data-picture-mapping="weatherhelm_breaks" src="/sites/default/files/m_union.jpg" /> <span class="card-title">Education</span></div>
+			<div class="card-content grey-text">
+				<div class="card-title grey-text">University Of Michigan
+					<div class="card-icon right"><i class="mdi-social-school"></i></div>
+				</div>
+				<h6><strong>Bachelor of Science in Engineering,</strong> Computer Engineering, <em>2011</em></h6>
+			</div>
+		</div>
+	</div>
+	<div class="col s12 m12 l12">
+		<div class="card">
+			<div class="card-content grey-text">
+				<div class="card-title grey-text">Skills
+					<div class="card-icon right"><i class="mdi-image-flash-on"></i></div>
+				</div>
+				<div class="section">
+					<div class="row">
+						<div class="col">
+							<h5>Languages</h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col s4 l2"><a onclick="Materialize.toast('C#', 4000)"><img alt="C#" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/c_sharp.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Objective C', 4000)"><img alt="Objective C" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/obj_c.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Java', 4000)"><img alt="Java" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/java.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Swift', 4000)"><img alt="Swift" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/swift.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('C', 4000)"><img alt="C" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/c.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('C++', 4000)"><img alt="C++" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/c++.png" /></a></div>
+					</div>
+					<div class="row">
+						<div class="col s4 l2"><a onclick="Materialize.toast('HTML', 4000)"><img alt="HTML" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/html5.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('CSS', 4000)"><img alt="CSS" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/css3.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('LESS', 4000)"><img alt="LESS" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/less.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('SASS', 4000)"><img alt="SASS" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/sass.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Javascript', 4000)"><img alt="Javascript" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/js.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Jquery', 4000)"><img alt="Jquery" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/jquery.png" /></a></div>
+					</div>
+					<div class="row">
+						<div class="col s4 l2"><a onclick="Materialize.toast('PHP', 4000)"><img alt="PHP" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/php.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Python', 4000)"><img alt="Python" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/python.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Ruby', 4000)"><img alt="Ruby" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/ruby.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('JSON', 4000)"><img alt="JSON" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/json.png" /></a></div>
+					</div>
+				</div>
+				<div class="divider">&nbsp;</div>
+				<div class="section">
+					<div class="row">
+						<div class="col">
+							<h5>Platforms</h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col s4 l2"><a onclick="Materialize.toast('Android', 4000)"><img alt="Android" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/android.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('iOS', 4000)"><img alt="iOS" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/ios.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Windows', 4000)"><img alt="Windows" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/windows.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('OS X', 4000)"><img alt="OS X" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/os_x.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Drupal', 4000)"><img alt="Drupal" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/drupal.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Magento', 4000)"><img alt="Magento" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/magento.png" /></a></div>
+					</div>
+				</div>
+				<div class="divider">&nbsp;</div>
+				<div class="section">
+					<div class="row">
+						<div class="col">
+							<h5>Software</h5>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col s4 l2"><a onclick="Materialize.toast('Xamarin Studio', 4000)"><img alt="Xamarin Studio" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/xamarin.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Xcode', 4000)"><img alt="Xcode" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/xcode.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Android Studio', 4000)"><img alt="Android Studio" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/a_studio.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Photoshop', 4000)"><img alt="Photoshop" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/photoshop.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Microsoft Office', 4000)"><img alt="Microsoft Office" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/office.png" /></a></div>
+						<div class="col s4 l2"><a onclick="Materialize.toast('Git, SVN, &amp; Mercurial', 4000)"><img alt="Git, SVN, &amp; Mercurial" class="responsive-img" data-picture-mapping="6_column" src="/sites/default/files/skills/git.png" /></a></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!--<span class="card-icon right"><i class="mdi-social-person"></i><i class="mdi-social-public"></i><i class="mdi-hardware-computer"></i><i class="mdi-social-school"></i><i class="mdi-image-flash-on"></i></span>-->
